4ba1ccb342101efe0f602168394b498d2c8d589c
Jystro <35638179+Jystro@users.noreply.github.com>
2021-12-18 22:24:56 +0100

    Removed unnecessary comment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
 import React from "react";
 import Countdown from "./Countdown";
-// class Cdountdown extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { date: new Date() };
-//     }
-
-//     componentDidMount() {
-//         this.timerID = setInterval(() => this.tick(), 1000);
-//     }
-
-//     componentWillUnmount() {
-//         clearInterval(this.timerID);
-//     }
-
-//     tick() {
-//         this.setState({ date: new Date() });
-//     }
-
-//     render() {
-//         return (
-//             <div className='Countdown'>
-//                 <h1>Mancano {this.state.date} al nuovo anno</h1>
-//             </div>
-//         );
-//     }
-//}
 
 function App() {
   return (
